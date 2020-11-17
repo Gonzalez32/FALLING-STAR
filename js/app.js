0369c9d6458cc433cfe3883576b6cc4b4902126d
Gonzalez32 <3232rose@gmail.com>
2020-11-17 11:32:02 -0600

    render gamebox and ball

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// user story: move the ball threw the gaps within the blocks as they go upwards.
+
+// G-L: create an eventlistener on the key's left & right. Wirte out a function where the ball hit's up they lose/break the game as an error.
+
