252b8562d7d27af7e8116c997d90ad38b86fcac9
Gonzalez32 <3232rose@gmail.com>
2020-11-18 01:37:57 -0600

    better control flow with ball moving

--- js/app.js
+++ js/app.js
@@ -7,8 +7,10 @@
 const ball = document.getElementById('ball')
 let interval;
 const gameBox = document.getElementById('gamebox')
-const bothKeys = 0
+let bothKeys = 0
 const currentBlocks = []
+
+
 // set my Interval for myTimer function for every 1 second.
 const timer = setInterval(myTimer, 1000);
 // this is my time fucntion that renders on the page for the user. 
@@ -17,24 +19,28 @@ function myTimer() {
     let t = d.toLocaleTimeString()
     document.getElementById('time').innerHTML = t
 }
+
 // this allows me to see which key is bring selective with console.log ---just for testing---
 // window.addEventListener('keydown', (e) => {
 //     console.log(e)
 // }) 
 
-// E-L when user is pressing left arrow or right arrow
-document.addEventListener('keydown', e => {
+// E-L on keybored but only when user is pressing left arrow or right arrow
+document.addEventListener('keydown', (e) => {
     if (bothKeys == 0) {
-        // bothKeys++
-        if (e.key === 'ArrowLeft') {
+        bothKeys++
+        // use keyCode: 37 = ArrowLeft
+        if (e.keyCode === 37) {
             interval = setInterval(left, 1)
         }
-        if (e.key === 'ArrowRight') {
+        // use keyCode: 39 = ArrowRight
+        if (e.keyCode === 39) {
             interval = setInterval(right, 1)
         }
     }
 })
 
+// within setInterval function left for my left-key set to 0 at left div-box 
 function left() {
     let left = parseInt(window.getComputedStyle(ball).getPropertyValue("left"))
     if (left > 0) {
@@ -42,6 +48,7 @@ function left() {
     }
 }
 
+// within setInterval function right for my right-key set to 475 at the right div-box
 function right() {
     let left = parseInt(window.getComputedStyle(ball).getPropertyValue("left"))
     if (left < 475) {
@@ -49,6 +56,7 @@ function right() {
     }
 }
 
+// add E-L so when keys are not in use or both keys are press in the sametime it stops the ball
 document.addEventListener('keyup', (e) => {
     clearInterval(interval)
     bothKeys=0
