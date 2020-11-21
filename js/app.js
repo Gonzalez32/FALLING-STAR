a9e21a6488246637260d25547d2c11613a2815de
Gonzalez32 <3232rose@gmail.com>
2020-11-20 20:35:55 -0600

    change the render outlook

--- js/app.js
+++ js/app.js
@@ -1,42 +1,24 @@
 // user story: move the ball threw the gaps within the blocks as they go upwards.
-
-// G-L: create an eventlistener on the key's left & right. Wirte out a function where the ball hit's up they lose/break the game as an error.
-
 /*-------------------------------- Constants --------------------------------*/
+const timer = setInterval(myWatch, 1000)
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-/*------------------------ Cached Element References ------------------------*/
-
-/*----------------------------- Event Listeners -----------------------------*/
-
-/*-------------------------------- Functions --------------------------------*/
-
-// first get the element by id and then create an E-L
-
-const ball = document.getElementById('ball')
-const gameBox = document.getElementById('gamebox')
-const message = document.getElementById('message')
-const play = document.getElementById('amazing')
-let control;
+let control
 let bothKeys = 0
 // keeps track setInterval function
 var tracker = 0
 var currentBars = []
 
-// set my Interval for myTimer function for every 1 second.
-const timer = setInterval(myWatch, 1000);
-// this is my time fucntion that renders on the page for the user. 
-function myWatch() {
-    let d = new Date()
-    let t = d.toLocaleTimeString()
-    document.getElementById('time').innerHTML = t
-}
+/*------------------------ Cached Element References ------------------------*/
+const ball = document.getElementById('ball')
+const gameBox = document.getElementById('gamebox')
+const message = document.getElementById('message')
 
+/*----------------------------- Event Listeners -----------------------------*/
 // this allows me to see which key is bring selective with console.log ---just for testing---
 // window.addEventListener('keydown', (e) => {
-//     console.log(e)
-// }) 
+    //     console.log(e)
+// })
 
 // E-L on keybored but only when user is pressing left arrow or right arrow
 document.addEventListener('keydown', (e) => {
@@ -52,6 +34,14 @@ document.addEventListener('keydown', (e) => {
         }
     }
 })
+
+// add E-L so when keys are not in use or both keys are press in the sametime it stops the ball
+document.addEventListener('keyup', (e) => {
+    clearInterval(control)
+    bothKeys=0
+})
+
+/*-------------------------------- Functions --------------------------------*/
 
 // within setInterval function left for my left-key set to 0 at left div-box 
 function left() {
@@ -69,21 +59,10 @@ function right() {
     }
 }
 
-// add E-L so when keys are not in use or both keys are press in the sametime it stops the ball
-document.addEventListener('keyup', (e) => {
-    clearInterval(control)
-    bothKeys=0
-})
-
-// hard reset button at the moment
-function resetButton() {
-    location.reload()
-}
 // need to create an element div for the bar to render on the page. Within the bar there should be gaps as well
 
-
 // S-I function to render bar and gap continuously
-let bars = setInterval(function(){
+let play = setInterval(function() {
     var lastBar = document.getElementById('bar' + (tracker - 1))
     var lastGap = document.getElementById('gap' + (tracker - 1))
     if (tracker > 0) {
@@ -113,10 +92,11 @@ let bars = setInterval(function(){
     
     var ballTop = parseInt(window.getComputedStyle(ball).getPropertyValue('top'))
     var ballLeft = parseInt(window.getComputedStyle(ball).getPropertyValue('left'))
+    
     let drop = 0
     if (ballTop <= 0) {
         message.innerHTML = `GAME OVER SCORE: ${tracker-9}`
-        clearInterval(bars)
+        clearInterval(play)
     }
     // need to render the bars upward to the div gamebox 
     for (var i = 0; i < currentBars.length; i++) {
@@ -125,17 +105,17 @@ let bars = setInterval(function(){
         let igap = document.getElementById('gap' + current)
         let ibarTop = parseFloat(window.getComputedStyle(ibar).getPropertyValue('top'))
         let igapLeft = parseFloat(window.getComputedStyle(igap).getPropertyValue('left'))
-        ibar.style.top = ibarTop - 0.5 + 'px';
-        igap.style.top = ibarTop - 0.5 + 'px';
+        ibar.style.top = ibarTop - 0.5 + 'px'
+        igap.style.top = ibarTop - 0.5 + 'px'
         if (ibarTop < -20) {
-            currentBars.shift();
-            ibar.remove();
-            igap.remove();
+            currentBars.shift()
+            ibar.remove()
+            igap.remove()
         }
         if (ibarTop - 20 < ballTop && ibarTop > ballTop) {
             drop++;
             if (igapLeft <= ballLeft && igapLeft + 20 >= ballLeft) {
-                drop = 0;
+                drop = 0
             }
         }
     }
@@ -146,5 +126,17 @@ let bars = setInterval(function(){
     } else {
         ball.style.top = ballTop - 0.5 + 'px'
     }
-},1)
+}, 1)
 
+// set my Interval for myTimer function for every 1 second.
+// this is my time fucntion that renders on the page for the user. 
+function myWatch() {
+    let d = new Date()
+    let t = d.toLocaleTimeString()
+    document.getElementById('time').innerHTML = t
+}
+
+// hard reset button
+function resetButton() {
+    location.reload()
+}
